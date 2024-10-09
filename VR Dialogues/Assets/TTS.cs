30608d2f0321a3742369e6671c81ab0f1e2f8191
Alen Jo <jo.alen1@outlook.com>
2024-10-09 11:33:39 -0500

    Updated TTS.cs
    Now I added the dropdown functionality to customize more of the ECAs by giving voice options and language codes. Language codes allow for a native language (other than English) to have precise pronounciation and ennunciation from the Amazon Polly service.
    
    Hopefully this works :D

--- VR Dialogues/Assets/TTS.cs
+++ VR Dialogues/Assets/TTS.cs
@@ -14,14 +14,18 @@ using UnityEngine.UI;
 public class TTS : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private Dropdown voiceDropdown;
+    public enum PollyVoices { Aditi, Amy, Astrid, Bianca, Brian, Camila, Carla, Carmen, Celine, Chantal, Conchita, Cristiano, Dora, Emma, Enrique, Ewa, Filiz, Gabrielle, Geraint, Giorgio, Gwyneth, Hans, Ines, Ivy, Jacek, Jan, Joanna, Joey, Justin, Karl, Kendra, Kevin, Kimberly, Lea, Liv, Lotte, Lucia, Lupe, Mads, Maja, Marlene, Mathieu, Matthew, Maxim, Mia, Miguel, Mizuki, Naja, Nicole, Olivia, Penelope, Raveena, Ricardo, Ruben, Russell, Salli, Seoyeon, Takumi, Tatyana, Vicki, Vitoria, Zeina, Zhiyu, Aria, Ayanda, Arlet, Hannah, Arthur, Daniel, Liam, Pedro, Kajal, Hiujin, Laura, Elin, Ida, Suvi, Ola, Hala, Andres, Sergio, Remi, Adriano, Thiago, Ruth, Stephen, Kazuha, Tomoko, Niamh, Sofie, Lisa, Isabelle, Zayd, Danielle, Gregory, Burcu };
+    public enum PollyLanguageCodes { None, arb, cmn_CN, cy_GB, da_DK, de_DE, en_AU, en_GB, en_GB_WLS, en_IN, en_US, es_ES, es_MX, es_US, fr_CA, fr_FR, is_IS, it_IT, ja_JP, hi_IN, ko_KR, nb_NO, nl_NL, pl_PL, pt_BR, pt_PT, ro_RO, ru_RU, sv_SE, tr_TR, en_NZ, en_ZA, ca_ES, de_AT, yue_CN, ar_AE, fi_FI, en_IE, nl_BE, fr_BE }
+
+    [SerializeField] private PollyVoices voice;
+    [SerializeField] private PollyLanguageCodes languagecode;
 
     private async void Start()
     {
         string filePath = $"{Application.dataPath}/dialogue.txt";
         string textToSynthesize = File.ReadAllText(filePath);
-        //List<string> voiceOptions = Enum.GetNames(typeof(VoiceId)).ToList();
-        //PopulateVoiceDropdown(voiceOptions);
+
+
         if (string.IsNullOrEmpty(textToSynthesize))
         {
             Debug.LogError("Either the dialogue is empty or not found!");
@@ -30,11 +34,13 @@ public class TTS : MonoBehaviour
 
         var credentials = new BasicAWSCredentials("AKIA2NK3X4NVQCGYVBEE", "yF5jkrnJ2uEMcI/PkbsON4EYaPshsXo2NYPbbXSs");
         var client = new AmazonPollyClient(credentials, RegionEndpoint.USEast1);
+
         var request = new SynthesizeSpeechRequest()
         {
             Text = textToSynthesize,
             Engine = Engine.Neural,
-            VoiceId = VoiceId.Aria,
+            VoiceId = (VoiceId)Enum.Parse(typeof(VoiceId), voice.ToString()),
+            LanguageCode = languagecode == PollyLanguageCodes.None ? string.Empty : languagecode.ToString(),
             OutputFormat = OutputFormat.Mp3
         };
 
@@ -53,12 +59,6 @@ public class TTS : MonoBehaviour
                 audioSource.Play();
             }
         }
-    }
-
-    private void PopulateVoiceDropdown(List<string> voiceOptions)
-    {
-        voiceDropdown.ClearOptions();
-        voiceDropdown.AddOptions(voiceOptions);
     }
 
     private void WriteIntoFile(Stream stream)
