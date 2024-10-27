e7d3051de30132ecc6845ab07a5e3513a42842e1
Multimodal Interaction Lab <abbas.khawaja@gmail.com>
2024-10-27 15:52:30 -0500

    This is just before switching to URP

--- VR Dialogues/Assets/Hrishikesh/Scripts/PortalCamera.cs
+++ VR Dialogues/Assets/Hrishikesh/Scripts/PortalCamera.cs
@@ -8,11 +8,11 @@ public class PortalCamera : MonoBehaviour {
 	public Transform portal;
 	public Transform otherPortal;
 	public float rotationOffset;
-	private Vector3 startRotation;
+	private Transform startTransform;
 
     private void Start()
     {
-        startRotation = transform.eulerAngles;
+        startTransform = transform;
     }
 
     // Update is called once per frame
@@ -23,7 +23,7 @@ public class PortalCamera : MonoBehaviour {
         float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation) + rotationOffset;
 
         Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
-		Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+		Vector3 newCameraDirection = portalRotationalDifference * new Vector3(playerCamera.forward.x, playerCamera.forward.y, playerCamera.forward.z);
 		transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
 	}
 }
