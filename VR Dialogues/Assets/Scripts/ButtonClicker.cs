ca15f333574e8cdbce7a306d8eaf378512645bb0
Multimodal Interaction Lab <abbas.khawaja@gmail.com>
2024-10-26 21:07:21 -0500

    Button Work

--- VR Dialogues/Assets/Scripts/ButtonClicker.cs
+++ VR Dialogues/Assets/Scripts/ButtonClicker.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ButtonClicker : MonoBehaviour
 {
+    private Vector3 originalPos;
+    [SerializeField] Vector3 maxPress;
+    [SerializeField] float distance;
+    private Vector3 otherPosition;
+    private GameObject otherGameObject;
     
     void Start()
     {
-        
+        originalPos = transform.position;
+        //otherPosition = Vector3.zero;
+        otherGameObject = null;
     }
 
     void Update()
     {
+        if (otherGameObject != null)
+        {
+            if (Vector3.Distance(otherGameObject.transform.position, transform.position) >= distance)
+            {
+                transform.position = originalPos;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        otherGameObject = other.gameObject;
+        otherPosition = other.transform.position;
+        Debug.Log("The button is currently colliding. The distance is " + Vector3.Distance(otherPosition, transform.position));
+        transform.localPosition = maxPress;
         
     }
 
-    public void OnClick()
+    void OnTriggerExit(Collider other)
     {
-        Debug.Log("Button Clicked");
+        Debug.Log("The distance on trigger exit is " + Vector3.Distance(otherPosition, transform.position));
     }
+
+
 }
