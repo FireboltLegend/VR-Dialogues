a8236d5af2adc165ff4cd9a5fdd4f3a0133d63a4
Multimodal Interaction Lab <abbas.khawaja@gmail.com>
2024-10-27 20:29:59 -0500

    Button Progress

--- VR Dialogues/Assets/Scripts/ButtonClicker.cs
+++ VR Dialogues/Assets/Scripts/ButtonClicker.cs
@@ -1,45 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class ButtonClicker : MonoBehaviour
 {
     private Vector3 originalPos;
-    [SerializeField] Vector3 maxPress;
+    private Vector3 maxPress; // 0.26 down from the original position
     [SerializeField] float distance;
-    private Vector3 otherPosition;
     private GameObject otherGameObject;
+    private GameObject wardrobeObject;
+    private Vector3 originalWardrobePos;
+    [SerializeField] Transform hiddenPos;
+    private bool canUnclick;
+    private bool canSummon;
+    [SerializeField] float speed;
+    private bool isSummoning;
     
     void Start()
     {
-        originalPos = transform.position;
+        isSummoning = false;
+        canSummon = false;
+        originalPos = transform.localPosition;
         otherGameObject = null;
+        canUnclick = false;
+        maxPress = originalPos - new Vector3(0, 0.26f, 0);
+        wardrobeObject = GameObject.FindGameObjectWithTag("Wardrobe");
+        originalWardrobePos = wardrobeObject.transform.position;
     }
 
     void Update()
     {
-        if (otherGameObject != null)
+        if (otherGameObject != null && canUnclick)
         {
             if (Vector3.Distance(otherGameObject.transform.position, transform.position) >= distance)
             {
-                transform.position = originalPos;
+                transform.localPosition = originalPos;
             }
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        otherGameObject = other.gameObject;
-        Debug.Log("The button is currently colliding. The distance is " + Vector3.Distance(otherGameObject.transform.position, transform.position));
-        transform.localPosition = maxPress;
-        
+        if (!isSummoning)
+        {
+            canUnclick = false;
+            otherGameObject = other.gameObject;
+            Debug.Log("The button is currently colliding. The distance is " + Vector3.Distance(otherGameObject.transform.position, transform.position));
+            transform.localPosition = maxPress;
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
-        Debug.Log("The distance on trigger exit is " + Vector3.Distance(otherGameObject.transform.position, transform.position));
+        if (!isSummoning)
+        {
+            canUnclick = true;
+            Debug.Log("The distance on trigger exit is " + Vector3.Distance(otherGameObject.transform.position, transform.position));
+            canSummon = !canSummon;
+            SummonOrNot();
+        }
     }
 
+    void SummonOrNot()
+    {
+        isSummoning = true;
+        if (canSummon)
+        {
+            wardrobeObject.transform.position = Vector3.Lerp(originalWardrobePos, hiddenPos.position, speed);
+        }
+        else
+        {
+            wardrobeObject.transform.position = Vector3.Lerp(hiddenPos.position, originalWardrobePos, speed);
+        }
+        isSummoning = false;
+    }
 
 }
