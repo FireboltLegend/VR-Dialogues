c7cd6b8079f467f369f7337790ca1dd9c2a11d6d
Yadiel Valentin Rios <yadielvalentin1389@gmail.com>
2024-10-31 01:03:12 -0500

    ButtonClicker improved
    Hopefully finished

--- VR Dialogues/Assets/Scripts/ButtonClicker.cs
+++ VR Dialogues/Assets/Scripts/ButtonClicker.cs
@@ -13,104 +13,42 @@ public class ButtonClicker : MonoBehaviour
     private GameObject wardrobeObject;
     private Vector3 originalWardrobePos;
     [SerializeField] Transform hiddenPos;
-    private bool canUnclick;
-    private bool canSummon;
     [SerializeField] float speed;
-    private bool isSummoning;
 
 
     // New method variables
     private bool canPress;
     private bool isHere;
+    private bool waitForDistance;
 
     void Start()
     {
-        isSummoning = false;
-        canSummon = false;
         originalPos = transform.localPosition;
         otherGameObject = null;
-        canUnclick = false;
         maxPress = originalPos - new Vector3(0, 0.26f, 0);
         wardrobeObject = GameObject.FindGameObjectWithTag("Wardrobe");
         originalWardrobePos = wardrobeObject.transform.position;
 
         canPress = true;
-
         isHere = true;
+        waitForDistance = false;
     }
 
     void Update()
     {
-        /*
-        if (otherGameObject != null && canUnclick)
-        {
-            if (Vector3.Distance(otherGameObject.transform.position, transform.position) >= distance)
-            {
-                transform.localPosition = originalPos;
-            }
-            canUnclick = false;
-        }*/
-        /*if (otherGameObject != null && !canPress)
-        {
-            if (Vector3.Distance(otherGameObject.transform.position, transform.position) >= distance)
-            {
-                canPress = true;
-            }
-        }*/
+        WaitForDistance();
     }
 
     void OnTriggerEnter(Collider other)
     {
-        /*if (!isSummoning)
-        {
-            canUnclick = false;
-            otherGameObject = other.gameObject;
-            Debug.Log("The button is currently colliding. The distance is " + Vector3.Distance(otherGameObject.transform.position, transform.position));
-            transform.localPosition = maxPress;
-        }*/
         MoveWardrobe();
         canPress = false;
     }
 
     void OnTriggerExit(Collider other)
     {
-        /*
-        if (!isSummoning)
-        {
-            canUnclick = true;
-            Debug.Log("The distance on trigger exit is " + Vector3.Distance(otherGameObject.transform.position, transform.position));
-            canSummon = !canSummon;
-            SummonOrNot();
-        }*/
-        // while (Vector3.Distance(otherGameObject.transform.positi
         otherGameObject = other.gameObject;
-        WaitForDistance();
-    }
-
-    void SummonOrNot()
-    {
-        isSummoning = true;
-        if (canSummon)
-        {
-            wardrobeObject.transform.position = Vector3.Lerp(originalWardrobePos, hiddenPos.position, speed);
-        }
-        else
-        {
-            wardrobeObject.transform.position = Vector3.Lerp(hiddenPos.position, originalWardrobePos, speed);
-        }
-        isSummoning = false;
-    }
-
-    void SwitchState()
-    {
-        if (isHere)
-        {
-            transform.localPosition = maxPress;
-        }
-        else
-        {
-            transform.localPosition = originalPos;
-        }
+        waitForDistance = true;
     }
 
     void MoveWardrobe()
@@ -130,17 +68,35 @@ public class ButtonClicker : MonoBehaviour
         isHere = !isHere;
     }
 
+    void SwitchState()
+    {
+        if (isHere)
+        {
+            Debug.Log("Moving to pressed state");
+            transform.localPosition = maxPress;
+        }
+        else
+        {
+            Debug.Log("Moving to original state");
+            transform.localPosition = originalPos;
+        }
+    }
+
     void WaitForDistance()
     {
         float realDistance = -1.0f;
         Debug.Log("Beginning Distance Loop...");
-        while (realDistance < distance)
+        if (waitForDistance)
         {
             Debug.Log("Real Distance is " + realDistance);
             realDistance = Vector3.Distance(otherGameObject.transform.position, transform.position);
         }
-        canPress = true;
-        Debug.Log("Distance achieved!");
+        if (realDistance >= distance)
+        {
+            canPress = true;
+            Debug.Log("Distance achieved!");
+            waitForDistance = false;
+        }
     }
 
 }
