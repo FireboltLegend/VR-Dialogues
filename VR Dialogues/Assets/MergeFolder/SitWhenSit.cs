d8b5aa8214f0d2cc01ec65330b53ef8ae9cd80e3
EvanOlson31 <58322629+spookyboi31@users.noreply.github.com>
2024-11-03 17:30:22 -0600

    Merge folder created

---
+++ VR Dialogues/Assets/MergeFolder/SitWhenSit.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class SitWhenSit : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 positionHigh;
+    public Vector3 positionLow;
+    public float timer = 0f;
+    public bool startTimer = false;
+    // Update is called once per frame
+
+    void Update()
+    {
+        if(player.transform.position.x > positionLow.x && player.transform.position.x < positionHigh.x && player.transform.position.y > positionLow.y && player.transform.position.y < positionHigh.y && player.transform.position.z > positionLow.z && player.transform.position.z < positionHigh.z)
+        {
+            this.gameObject.GetComponent<Animator>().SetBool("Sitting", true);
+        }
+        else
+        {
+            this.gameObject.GetComponent<Animator>().SetBool("Sitting", false);
+        }
+        if (startTimer)
+        {
+            timer += Time.deltaTime;
+            if (timer > 10f)
+            {
+                startTimer = false;
+                timer = 0;
+                this.gameObject.GetComponent<Animator>().SetBool("Mad", false);
+            }
+        }
+    }
+}
