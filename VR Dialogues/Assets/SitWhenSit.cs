cf15f19a756ddee87d51670eb7200fb5980cf7be
EvanOlson31 <58322629+spookyboi31@users.noreply.github.com>
2024-11-03 14:37:43 -0600

    Skins all set up.

--- VR Dialogues/Assets/SitWhenSit.cs
+++ VR Dialogues/Assets/SitWhenSit.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class SitWhenSit : MonoBehaviour
 {
     public GameObject player;
-    public Animator animator;
     public Vector3 positionHigh;
     public Vector3 positionLow;
     public float timer = 0f;
@@ -17,11 +16,11 @@ public class SitWhenSit : MonoBehaviour
     {
         if(player.transform.position.x > positionLow.x && player.transform.position.x < positionHigh.x && player.transform.position.y > positionLow.y && player.transform.position.y < positionHigh.y && player.transform.position.z > positionLow.z && player.transform.position.z < positionHigh.z)
         {
-            animator.SetBool("Sitting", true);
+            this.gameObject.GetComponent<Animator>().SetBool("Sitting", true);
         }
         else
         {
-            animator.SetBool("Sitting", false);
+            this.gameObject.GetComponent<Animator>().SetBool("Sitting", false);
         }
         if (startTimer)
         {
