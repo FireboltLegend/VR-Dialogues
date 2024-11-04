172c017268809f76008376fa82c75a34ae757edb
Multimodal Interaction Lab <hrishikeshs2005@gmail.com>
2024-11-03 19:15:22 -0600

    Button to Summon Wardrobe

---
+++ VR Dialogues/Assets/MergeFolder/Scripts/SendWardrobeBack.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendWardrobeBack : MonoBehaviour
+{
+    [SerializeField] Transform originalTransform;
+    [SerializeField] float speed;
+
+    [SerializeField] float buttonPressLength;
+    Vector3 pressedPosition;
+
+    GameObject wardrobeObject;
+    bool canLerp;
+
+    void Start()
+    {
+        pressedPosition = transform.localPosition - new Vector3(0, buttonPressLength, 0);
+        wardrobeObject = gameObject.transform.parent.gameObject.transform.parent.gameObject; // grandparent object
+        canLerp = false;
+    }
+
+    void Update()
+    {
+        if (canLerp) 
+        {
+            wardrobeObject.transform.position = Vector3.Lerp(wardrobeObject.transform.position, originalTransform.position, speed);
+        }
+        if (wardrobeObject.transform.position == originalTransform.position)
+            canLerp = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Button has been pressed");
+        transform.localPosition = pressedPosition;
+        canLerp = true;
+    }
+}
