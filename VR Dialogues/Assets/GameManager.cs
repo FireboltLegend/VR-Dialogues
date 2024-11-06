074c3087ad8c0fd1f737d464c7b40b0cb167fe8e
EvanOlson31 <58322629+spookyboi31@users.noreply.github.com>
2024-11-05 19:05:35 -0600

    Merge between Yadiel+Evan and Hrishikesh further fixed

---
+++ VR Dialogues/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool happy;
+    public bool sad;
+    public bool confused;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
