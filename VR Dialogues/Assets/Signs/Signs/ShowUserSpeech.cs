2b5e17d290e26b079173c6cc68be7276ead36d4f
Multimodal Interaction Lab <abbas.khawaja@gmail.com>
2024-11-11 17:46:40 -0600

    Added thinking animation
    Got a thinking animation script working and updating the ECAs.
    
    TODO:
    - Fix the thinking animation (or the Animator and overhaul)
    - Fix the prompt responses -- seems GPT-ish

--- VR Dialogues/Assets/Signs/Signs/ShowUserSpeech.cs
+++ VR Dialogues/Assets/Signs/Signs/ShowUserSpeech.cs
@@ -4,16 +4,21 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class ShowUserSpeech : MonoBehaviour
 {
     [SerializeField] private TMP_Text speechText;
     [SerializeField] private string filePath;
-
+    [SerializeField] private GameObject boy;
+    [SerializeField] private GameObject girl;
+    private bool thinkingEnabled;
     // Start is called before the first frame update
     void Start()
     {
         filePath = Path.Combine(Application.dataPath, "speaker.txt");
+        File.WriteAllText(filePath, "");
+        thinkingEnabled = false;
     }
 
     // Update is called once per frame
@@ -27,7 +32,31 @@ public class ShowUserSpeech : MonoBehaviour
             if (speech.Equals("Say Something!")) 
             {
                 speechText.outlineWidth = 0.2f;
+
+                if (!thinkingEnabled)
+                { 
+                    PlayAnimation(boy, "Base Layer.thinking");
+                    PlayAnimation(girl, "Base Layer.thinking");
+                    thinkingEnabled = true;
+                }
+            }
+            else { 
+                thinkingEnabled = false;
             }
         }
+    }
+
+    public void PlayAnimation(GameObject eca, string stateAnimation)
+    {
+        Animator[] animators = eca.GetComponentsInChildren<Animator>(true);
+        
+        foreach (Animator animator in animators)
+        {
+            if (animator.gameObject.activeInHierarchy)
+            {
+                Debug.Log($"Playing animation {stateAnimation} on {animator.gameObject.name}");
+                animator.Play(stateAnimation);
+            }
+        }     
     }
 }
